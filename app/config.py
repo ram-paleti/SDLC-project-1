8cafa2cc28362ff239d4fa315274a2f5aa12770b
ram-paleti <paletiramprasad.paleti@gmail.com>
2025-07-06 19:18:21 +0530

    Delete app directory

--- app/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # Load from .env file
-
-WATSONX_API_KEY = os.getenv("WATSONX_API_KEY")
-WATSONX_PROJECT_ID = os.getenv("WATSONX_PROJECT_ID")
-WATSONX_MODEL_ID = os.getenv("WATSONX_MODEL_ID")
-
